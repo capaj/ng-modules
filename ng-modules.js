80b230a47cd2fed9d2135d13abe1c33acef11bd3
Jiri Spac <jiri.spac@ys.cz>
2014-09-16 02:14:26 +0200

    at least it can glob js files now

--- ng-modules.js
+++ ng-modules.js
@@ -1,18 +1,49 @@
 var fs = require('fs');
 require('array-sugar');
+
+var glob = require('glob');
 var createModule = function(pathToJSON) {
-	var moduleDef = JSON.parse(fs.readFileSync(pathToJSON, 'utf8'));
-	if (moduleDef.dep) {
-		
+	var relativeTo = pathToJSON.substring(0, pathToJSON.lastIndexOf('/') + 1);
+	var cwd = process.cwd();
+	process.chdir(relativeTo);
+	var moduleString;
+	try{
+		moduleString = fs.readFileSync(pathToJSON.substring(pathToJSON.lastIndexOf('/') + 1), 'utf8');
+	}catch(err){
+		console.error("Error reading file " + pathToJSON);
+		throw err;
 	}
+	console.log("parsing JSON: " + pathToJSON);
+
+	var moduleDef = JSON.parse(moduleString);
+	var scriptsArr = [];
+
+	if (Array.isArray(moduleDef.submodules)) {
+		scriptsArr = scriptsArr.concat(moduleDef.submodules.forEach(createModule));
+	}
+
+	if (Array.isArray(moduleDef.globs)) {
+		moduleDef.globs.forEach(function (globExp){
+			var files = glob.sync(globExp);
+			files.forEach(function (file, i){
+			    files[i] = relativeTo + file;
+			});
+			scriptsArr = scriptsArr.concat(files);
+		});
+	}
+
+	process.chdir(cwd);
+
+	return scriptsArr;
 };
 
 /**
  * @param {String} filename
+ * @param {String} to name of the file to create
  */
-module.exports = function(filename) {
-	var scriptsToAdd = [];
-
+module.exports = function(filename, to) {
+	var scriptElements = '';
+	var relativeToHtml = filename.substring(0, filename.lastIndexOf('/') + 1);
 	var f = fs.readFileSync(filename, 'utf8');
 	var splitted = f.split(/<ng-module/g);
 	if (splitted.last.length === 0) {
@@ -24,14 +55,19 @@ module.exports = function(filename) {
 		if (srcIndex !== -1) {
 			//this is a proper module element with src attribute
 			item = item.substr(srcIndex + 5);
-			var path = item.substr(0, item.indexOf('"'));
-			console.log("loading JSON: " + path);
-			var scripts = createModule(path);
+			var path = relativeToHtml + item.substr(0, item.indexOf('"'));
 
+			var scripts = createModule(path, relativeToHtml);
+			scripts.forEach(function (script){
+			    scriptElements += '<script src="' + script + ' "></script>'
+			});
 
 		} else {
 
 		}
 	}
+
+	console.log("all script elements added are");
+	console.log(scriptElements);
 
 };
--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
 var ngModules = require('../ng-modules');
 
+var fs = require('fs');
+
 module.exports = {
 	basic: function() {
-		ngModules('./test/index.html');
+		ngModules('./test/index_template.html', 'index.html');
+
+		var ind = fs.readFileSync('./test/index.html');
+		expect(ind.indexOf('<script src="1.js">')).to.not.equal(-1);
+
 	}
 };
